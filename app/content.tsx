921a5c394323f8e73f2d5f5846c622dd09467f65
wesjrose-gbc <101625918@georgebrown.ca>
2025-05-10 12:53:00 +0000

    feat: update content and page to complete e2

--- app/content.tsx
+++ app/content.tsx
@@ -1,10 +1,104 @@
 "use client"
 
-import React from "react";
+import React, { PureComponent } from "react";
+import ReactPlayer from 'react-player';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import "leaflet/dist/leaflet.css"
+
+
+const data = [
+    {
+        name: 'Hello world',
+        uv: 8000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
+
+const position:[number, number] = [51.505, -0.09]
 
 export default function Content() {
 
     return <>
-        <h1>Hello, Next.js!</h1>
+        <div className="container text-center">
+            <div className="row">
+                <div className="col-6">
+                    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{height: 400}}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={position}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>,
+                </div>
+                <div className="col-6">
+                    <ResponsiveContainer width="100%" height="100%" minHeight={300}>
+                        <LineChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
+            <div className="col-12">
+                <ReactPlayer url='https://soundcloud.com/mbbofficial/journey?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing' />
+            </div>
+        </div>
     </>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import dynamic from "next/dynamic";
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 export default function Page() {
 
